9769733d1e84742e108937be09d7487da60dc42f
Dinmukhamed Nurkaliyev@proton.me <dinmukhamednurkaliyev@proton.me>
2026-07-13 16:31:29 +0500

    chore: initial project setup

---
+++ nuxt.config.ts
@@ -0,0 +1,18 @@
+export default defineNuxtConfig({
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+  css: ["~/assets/css/main.css"],
+  modules: ["@pinia/nuxt", "@nuxt/eslint"],
+  vite: {
+    plugins: [(await import("@tailwindcss/vite")).default()],
+  },
+  typescript: {
+    typeCheck: true,
+    strict: true,
+  },
+  eslint: {
+    config: {
+      stylistic: true,
+    },
+  },
+});
