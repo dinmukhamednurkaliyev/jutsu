14e605dfa8a2f63019dc94d2e9e9f2c0c170c2e1
Dinmukhamed Nurkaliyev <dinmukhamednurkaliyev@proton.me>
2026-07-13 20:36:07 +0500

    feat(layout): implement default layout with header, footer and logo (#1)

---
+++ app/layouts/default.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import BrandLogo from "~/components/BrandLogo.vue";
+</script>
+
+<template>
+  <div class="min-h-screen flex flex-col">
+    <header
+      class="w-full bg-background-secondary border-b border-background-secondary"
+    >
+      <div class="container mx-auto px-4 h-16 flex items-center">
+        <BrandLogo />
+      </div>
+    </header>
+
+    <main class="grow">
+      <slot />
+    </main>
+
+    <footer class="w-full py-8 bg-background-secondary mt-auto">
+      <div
+        class="container mx-auto px-4 text-center text-on-background-secondary text-sm"
+      >
+        © 2020-2026 Jut-su.net. ДжутСУ/ДжитСУ All Rights Reserved
+      </div>
+    </footer>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ app/pages/index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts"></script>
+
+<template>
+  <section class="py-20 text-center">
+    <h1 class="text-4xl font-bold mb-4 text-on-background-primary">
+      Добро пожаловать на Jut-su
+    </h1>
+    <p class="text-on-background-primary">Здесь будет список аниме</p>
+  </section>
+</template>
+
+<style scoped></style>
